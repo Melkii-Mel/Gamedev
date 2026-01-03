a95e9d2ecd2f32f44b691a07b58a5b869169092f
Melkii-Mel <130223452+Melkii-Mel@users.noreply.github.com>
2026-01-03 07:30:55 +0500

    Add Cacher
    Singleton for convenient resource caching

---
+++ Utils/Cacher.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace Utils;
+
+public static class Cacher
+{
+    private static readonly MemoryCache Cache = new(new MemoryCacheOptions());
+
+    public static T? GetOrCache<T>(string cacheKey, bool shouldCache, bool tryGetCached, Func<T> provider)
+    {
+        if (tryGetCached)
+        {
+            var cached = Cache.Get<T>(cacheKey);
+            if (cached != null) return cached;
+        }
+
+        var result = provider();
+
+        if (shouldCache)
+        {
+            Cache.Set(cacheKey, result);
+        }
+
+        return result;
+    }
+}
