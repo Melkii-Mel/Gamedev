478c0f2009f409e388696b648465ff96fcf27b09
Melkii-Mel <130223452+Melkii-Mel@users.noreply.github.com>
2026-01-03 07:22:23 +0500

    Add Vector2D extensions
    For now it's the Ceil and Floor functions

---
+++ Utils/Extensions/Vector2D.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Silk.NET.Maths;
+using static System.Math;
+
+namespace Utils.Extensions;
+
+public static class Vector2DExtensions
+{
+    public static Vector2D<int> Ceil(this Vector2D<float> v)
+    {
+        return new Vector2D<int>((int)Ceiling(v.X), (int)Ceiling(v.Y));
+    }
+    
+    public static Vector2D<int> Floor(this Vector2D<float> v)
+    {
+        return new Vector2D<int>((int)Math.Floor(v.X), (int)Math.Floor(v.Y));
+    }
+}
