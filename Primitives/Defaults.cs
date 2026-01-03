46fa34e2fee6d145de770a6c4805283419f6f105
Melkii-Mel <130223452+Melkii-Mel@users.noreply.github.com>
2026-01-03 13:14:47 +0500

    Add Defaults.cs
    Contain default values that are required for consistency and predictability of an API

---
+++ Primitives/Defaults.cs
@@ -0,0 +1,11 @@
+﻿using System.Data;
+using Silk.NET.Maths;
+
+namespace Primitives;
+
+public static class Defaults
+{
+    public static readonly Vector2D<float> CenterPivot = new(0.5f, 0.5f);
+    public static readonly Vector2D<float> TopLeftPivot = Vector2D<float>.Zero;
+    public static readonly Vector2D<float> DefaultPivot = CenterPivot;
+}
