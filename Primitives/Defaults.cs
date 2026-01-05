d7e1de4d3558a04050cd3e78ad1e16cbe7bcd621
Melkii-Mel <130223452+Melkii-Mel@users.noreply.github.com>
2026-01-05 11:04:49 +0500

    Add default tolerance values

--- Primitives/Defaults.cs
+++ Primitives/Defaults.cs
@@ -5,6 +5,9 @@ namespace Primitives;
 
 public static class Defaults
 {
+    public const float Tolerance = 1e-6f;
+    public const double DTolerance = 1e-9f;
+
     public static readonly Vector2D<float> CenterPivot = new(0.5f, 0.5f);
     public static readonly Vector2D<float> TopLeftPivot = Vector2D<float>.Zero;
     public static readonly Vector2D<float> DefaultPivot = CenterPivot;
