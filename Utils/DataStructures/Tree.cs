41622611097f51fc6bb0078c25f633f446235506
Melkii-Mel <130223452+Melkii-Mel@users.noreply.github.com>
2026-01-06 07:55:29 +0500

    Add Tree data structure

---
+++ Utils/DataStructures/Tree.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Utils.DataStructures;
+
+public class Tree<T> : IEnumerable<T>
+{
+    private List<Tree<T>> Children { get; } = [];
+    public T? Value { get; set; }
+
+    public void Add(Tree<T> node)
+    {
+        Children.Add(node);
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        if (Value != null)
+        {
+            yield return Value;
+        }
+        foreach (var child in Children)
+        {
+            foreach (var value in child)
+            {
+                yield return value;
+            }
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
