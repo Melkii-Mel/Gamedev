e9c98024dd9051131ac7128e632ae06aaf81c43b
Melkii-Mel <130223452+Melkii-Mel@users.noreply.github.com>
2026-01-06 07:56:35 +0500

    Add WeakAction class
    For subscribers to still be able to be garbage-collected

---
+++ Utils/WeakAction.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Utils.DataStructures;
+
+namespace Utils;
+
+public class WeakAction<TArg>
+{
+    private Tree<WeakReference<Action<TArg>>> _actions = [];
+
+    public WeakAction(Action<TArg> value)
+    {
+        _actions.Value = new WeakReference<Action<TArg>>(value);
+    }
+
+    public static WeakAction<TArg> operator +(WeakAction<TArg> a, WeakAction<TArg> b)
+    {
+        a._actions.Add(b._actions);
+        return a;
+    }
+
+    public void Invoke(TArg arg)
+    {
+        foreach (var action in _actions)
+        {
+            if (action.TryGetTarget(out var target))
+            {
+                target?.Invoke(arg);
+            }
+        }
+    }
+}
