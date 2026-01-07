b5477131694ec1d67240a91775edc3bf826ed674
Melkii-Mel <130223452+Melkii-Mel@users.noreply.github.com>
2026-01-07 20:53:07 +0500

    Add ObservableProperty

---
+++ Utils/Observables/ObservableProperty.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Utils.Observables;
+
+public class ObservableProperty<T>(T value)
+{
+    public event Action<PropertyChangedEventArgs<T>>? PropertyChanged;
+
+    private T _value = value;
+    public T Value
+    {
+        get => _value;
+        set
+        {
+            var old = _value;
+            _value = value;
+            PropertyChanged?.Invoke(new(old, value));
+        }
+    }
+}
+
+public record PropertyChangedEventArgs<T>(T Old, T New);
