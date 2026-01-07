bc2c19d9ee4093349182e000805b2aa7e30e1b77
Melkii-Mel <130223452+Melkii-Mel@users.noreply.github.com>
2026-01-07 20:41:16 +0500

    Add MultiBiMap
    One-To-Many Bidirectional Multimap

---
+++ Utils/DataStructures/MultiBiMap.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace Utils.DataStructures;
+
+public class MultiBiMap<TKey, TValue>
+{
+    public ReadOnlyDictionary<TKey, TValue> KeyToValue { get; }
+    public ReadOnlyDictionary<TValue, List<TKey>> ValueToKeys { get; }
+    
+    private Dictionary<TKey, TValue> ktv = [];
+    private Dictionary<TValue, List<TKey>> vtks = [];
+
+    public MultiBiMap()
+    {
+        KeyToValue = new(ktv);
+        ValueToKeys = new(vtks);
+    }
+
+    public void Add(TKey key, TValue value)
+    {
+        ktv.Add(key, value);
+        if (vtks.TryGetValue(value, out var keys))
+        {
+            keys.Add(key);
+        }
+        else
+        {
+            vtks.Add(value, [key]);
+        }
+    }
+}
