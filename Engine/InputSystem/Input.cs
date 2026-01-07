b087affaee8e13055c218b356326d6163b683881
Melkii-Mel <130223452+Melkii-Mel@users.noreply.github.com>
2026-01-07 21:48:06 +0500

    Add Input.cs
    Wrapper around input API and Action system, part of an Engine

---
+++ Engine/InputSystem/Input.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Gamedev.InputSystem.ActionSystem;
+using Gamedev.InputSystem.Api;
+
+namespace Gamedev.InputSystem;
+
+public class Input(IInput devices)
+{
+    public IInput Devices { get; } = devices;
+
+    public InputActionDispatcher Dispatcher { get; } = new(devices);
+    public void RegisterActions(IEnumerable<InputAction> inputActions)
+    {
+        Dispatcher.RegisterActions(inputActions);
+    }
+}
