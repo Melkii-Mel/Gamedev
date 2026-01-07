305f7e14d305e20d1029c7686542954e361f8180
Melkii-Mel <130223452+Melkii-Mel@users.noreply.github.com>
2026-01-07 20:37:17 +0500

    Add TypeMap.cs
    Allows storing objects with their type acting as a key

---
+++ Utils/TypeMap.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace Utils;
+
+public class TypeMap
+{
+    private readonly Dictionary<Type, object> _map = [];
+
+    public T Set<T>(T value) where T : notnull
+    {
+        _map.Add(typeof(T), value);
+        return value;
+    }
+
+    public T? TryGet<T>() where T : notnull
+    {
+        return _map.TryGetValue(typeof(T), out var value) ? (T)value : default;
+    }
+
+    public T GetOrCreate<T>(Func<T> callback) where T : notnull
+    {
+        return TryGet<T>() ?? Set(callback());
+    }
+
+    public T GetOrCreate<T>() where T : notnull, new()
+    {
+        return GetOrCreate(() => new T());
+    }
+}
