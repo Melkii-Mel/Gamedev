3c2a95a523593d072ae520303b6385bccc64ab33
Melkii-Mel <130223452+Melkii-Mel@users.noreply.github.com>
2026-01-07 20:57:53 +0500

    Split contstructor with optional parameter into two
    Allows for 'new()' generic constraint, improves readability

--- Utils/WeakAction.cs
+++ Utils/WeakAction.cs
@@ -7,9 +7,10 @@ public class WeakAction<TArg>
 {
     private Tree<WeakReference<Action<TArg>>> _actions = [];
 
-    public WeakAction(Action<TArg>? value = null)
+    public WeakAction() { }
+    public WeakAction(Action<TArg> value)
     {
-        _actions.Value = value == null ? null : new WeakReference<Action<TArg>>(value);
+        _actions.Value = new WeakReference<Action<TArg>>(value);
     }
 
     public static WeakAction<TArg> operator +(WeakAction<TArg> a, WeakAction<TArg> b)
