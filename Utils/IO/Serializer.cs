7866d231bdcddc6028dccefb9820708b285bf30b
Melkii-Mel <130223452+Melkii-Mel@users.noreply.github.com>
2026-01-10 03:11:28 +0500

    Add Serializer utility

---
+++ Utils/IO/Serializer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Utils.IO;
+
+public static class Serializer
+{
+    /// <summary>
+    /// Optimized for saving save data defined as simple `record` DTOs.
+    /// </summary>
+    public static JsonSerializerOptions DefaultSerializerOptions = new()
+    {
+        IgnoreReadOnlyFields = true,
+        IgnoreReadOnlyProperties = true,
+    };
+
+    public static string Serialize<T>(T value, JsonSerializerOptions? optionsOverride = null) where T : notnull
+    {
+        return JsonSerializer.Serialize(value, optionsOverride ?? DefaultSerializerOptions);
+    }
+
+    public static T? Deserialize<T>(string json)
+    {
+        return JsonSerializer.Deserialize<T>(json, DefaultSerializerOptions);
+    }
+}
