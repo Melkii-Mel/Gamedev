7dc7fa77769eb5edaf075ef8bdb18f65db730f33
Melkii-Mel <130223452+Melkii-Mel@users.noreply.github.com>
2026-01-09 21:31:49 +0500

    Add unimplemented IsConcave method to the Polygon

--- Primitives/Shapes/Polygon.cs
+++ Primitives/Shapes/Polygon.cs
@@ -1,5 +1,12 @@
-﻿using Silk.NET.Maths;
+﻿using System;
+using Silk.NET.Maths;
 
 namespace Primitives.Shapes;
 
-public record Polygon(Vector2D<float>[] Points) : IShape2D;
+public record Polygon(Vector2D<float>[] Points) : IShape2D
+{
+    public bool IsConcave()
+    {
+        throw new NotImplementedException();
+    }
+}
