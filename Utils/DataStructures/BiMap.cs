5057593bae7d42af52c9bc6421c3ef42fa155797
Melkii-Mel <130223452+Melkii-Mel@users.noreply.github.com>
2026-01-09 20:51:05 +0500

    Add BiMap

---
+++ Utils/DataStructures/BiMap.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace Utils.DataStructures;
+
+public class BiMap<TKey, TValue>
+{
+    public ReadOnlyDictionary<TKey, TValue> KeyToValue { get; }
+    public ReadOnlyDictionary<TValue, TKey> ValueToKeys { get; }
+
+    private Dictionary<TKey, TValue> ktv = [];
+    private Dictionary<TValue, TKey> vtks = [];
+
+    public BiMap()
+    {
+        KeyToValue = new(ktv);
+        ValueToKeys = new(vtks);
+    }
+
+    public void Add(TKey key, TValue value)
+    {
+        ktv.Add(key, value);
+        vtks.Add(value, key);
+    }
+}
