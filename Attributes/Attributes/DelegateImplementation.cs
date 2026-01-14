f8599f7d19eb3ef2b343ae5557273f4fde00a5a7
Melkii-Mel <130223452+Melkii-Mel@users.noreply.github.com>
2026-01-14 20:42:10 +0500

    Define DelegateImplementation attribute

---
+++ Attributes/Attributes/DelegateImplementation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Attributes;
+
+[AttributeUsage(AttributeTargets.Class, Inherited = false, AllowMultiple = true)]
+public sealed class DelegateImplementationAttribute : Attribute
+{
+    public Type InterfaceType { get; }
+    public string TargetProperty { get; }
+
+    public DelegationStyle DelegationStyle { get; }
+
+    // See the attribute guidelines at 
+    // http://go.microsoft.com/fwlink/?LinkId=85236
+    public DelegateImplementationAttribute(Type interfaceType, string targetProperty, DelegationStyle delegationStyle = DelegationStyle.Implicit)
+    {
+        InterfaceType = interfaceType;
+        TargetProperty = targetProperty;
+        DelegationStyle = delegationStyle;
+    }
+}
+
+public enum DelegationStyle
+{
+    Implicit,
+    Explicit,
+}
