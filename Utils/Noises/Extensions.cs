b9a4c04d768eddcd7656b914fca435ec6b33b328
Melkii-Mel <130223452+Melkii-Mel@users.noreply.github.com>
2026-01-19 14:28:19 +0500

    Add Noise Utils

---
+++ Utils/Noises/Extensions.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using LibNoise;
+using LibNoise.Primitive;
+
+namespace Utils.Noises;
+
+public static class NoiseExtensions
+{
+    public const float P1D = 1;
+    public const float P2D = 0.7071067812f;
+    public const float P3D = 0.8660254038f;
+    public const float P4D = 1;
+    
+    public static float Sample(this PrimitiveModule noiseModule, AnyVector<float> position)
+    {
+        var v = position.Base;
+        var x = v.X;
+        var y = v.Y;
+        var z = v.Z;
+        var w = v.W;
+        
+        return noiseModule switch
+        {
+            Constant c => c.GetValue(x, y, z, w),
+            SimplexPerlin s => s.GetValue(x, y, z, w),
+            BevinsGradient g => g.GetValue(x, y, z),
+            BevinsValue b => b.GetValue(x, y, z),
+            Checkerboard c => c.GetValue(x, y, z),
+            Cylinders c => c.GetValue(x, y, z),
+            ImprovedPerlin p => p.GetValue(x, y, z).NormalizePerlin3D(),
+            Spheres s => s.GetValue(x, y, z),
+
+            _ => throw new NotSupportedException(),
+        };
+    }
+
+    private static float NormalizePerlin3D(this float value)
+    {
+        return (value / P3D + 0.955f) / 2;
+    }
+}
---
+++ Utils/Noises/NoiseDefaults.cs
@@ -0,0 +1,21 @@
+﻿using LibNoise;
+using LibNoise.Primitive;
+
+namespace Utils.Noises;
+
+public static class NoiseDefaults
+{
+    public static ImprovedPerlin Default(int seed = 0)
+    {
+        return new ImprovedPerlin
+        {
+            Seed = seed,
+            Quality = NoiseQuality.Best,
+        };
+    }
+    
+    public static TiledNoise<ImprovedPerlin> DefaultTiled(int seed = 0)
+    {
+        return new TiledNoise<ImprovedPerlin>(Default(seed));
+    }
+}
