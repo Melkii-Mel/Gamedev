9abb5f86805a14adeefeebbd8a18202eb7359b0e
Melkii-Mel <130223452+Melkii-Mel@users.noreply.github.com>
2026-01-28 18:49:25 +0500

    Pend WeakAction update until when it fully iterates through the tree

--- Utils/WeakAction.cs
+++ Utils/WeakAction.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Utils.DataStructures;
 
 namespace Utils;
@@ -6,6 +7,8 @@ namespace Utils;
 public class WeakAction<TArg>
 {
     private readonly Tree<WeakReference<Action<TArg>>> _actions = [];
+    private uint _isIterating;
+    private readonly List<WeakAction<TArg>> _pending = [];
 
     public WeakAction()
     {
@@ -18,14 +21,24 @@ public class WeakAction<TArg>
 
     public static WeakAction<TArg> operator +(WeakAction<TArg> a, WeakAction<TArg> b)
     {
-        a._actions.Add(b._actions);
+        if (a._isIterating > 0) a._pending.Add(b);
+        else a._actions.Add(b._actions);
         return a;
     }
 
     public void Invoke(TArg arg)
     {
+        _isIterating++;
         foreach (var action in _actions)
             if (action.TryGetTarget(out var target))
                 target?.Invoke(arg);
+        _isIterating--;
+        if (_isIterating == 0) FlushPended();
+    }
+
+    private void FlushPended()
+    {
+        foreach (var weakAction in _pending) _actions.Add(weakAction._actions);
+        _pending.Clear();
     }
 }
