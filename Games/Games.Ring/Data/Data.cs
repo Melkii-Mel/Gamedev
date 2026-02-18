3ed16600b0aeb3c48d77695e896394dbf4288018
Melkii-Mel <130223452+Melkii-Mel@users.noreply.github.com>
2026-02-19 02:38:14 +0500

    wip

---
+++ Games/Games.Ring/Data/Data.cs
@@ -0,0 +1,39 @@
+﻿#nullable enable
+using Primitives;
+
+namespace Games.Ring.Data;
+
+public record Data(
+    string AppName,
+    Rules Rules,
+    string[] LevelIds,
+    Sprites Sprites,
+    UiConfig Ui,
+    string LevelsDirectory,
+    string MusicDirectory
+);
+
+public record UiConfig(
+    ScoreScene ScoreScene,
+    string FontFamilyHeading,
+    string FontFamilyBody,
+    float FontSizeHeading,
+    float FontSizeBody,
+    Color[] DifficultyColors
+);
+
+public record ScoreScene(
+    int PerfectFontSize
+);
+
+public record Sprites(string Bullet);
+
+public record Rules(
+    int TopComboCount,
+    float PerfectValue,
+    float GoodValue,
+    float MissedValue,
+    float MaxScore,
+    float RingDistance,
+    float GoodTolerance
+);
---
+++ Games/Games.Ring/Level.cs
@@ -0,0 +1,36 @@
+﻿#nullable enable
+using System.Collections.Generic;
+using Utils.Engine;
+
+namespace Games.Ring;
+
+public record Level(
+    LevelHeading Heading,
+    LevelRing Ring,
+    LevelBody Body
+) : MenuUtils.GenericMenus.ILevel;
+
+public record LevelRing(List<Arc> Arcs);
+
+public record LevelHeading(
+    string Id,
+    string Name,
+    float Difficulty,
+    float Length,
+    string MusicId
+);
+
+public record LevelBody(
+    Pattern[] Patterns
+);
+
+public record Pattern(float Time, Formation Formation);
+
+public record Formation(
+    float AngleStart,
+    float AngleEnd,
+    float SpeedStart,
+    float SpeedEnd,
+    float Delay,
+    float Duration
+);
