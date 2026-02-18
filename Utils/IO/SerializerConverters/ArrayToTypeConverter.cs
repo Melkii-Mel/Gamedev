8123591183fd9e39bf196ea24a080b776bf58abd
Melkii-Mel <130223452+Melkii-Mel@users.noreply.github.com>
2026-02-19 02:13:04 +0500

    Add custom converters support for Serializer

---
+++ Utils/IO/SerializerConverters/ArrayToTypeConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Utils.IO.SerializerConverters;
+
+public static class ArrayToType
+{
+    public static object Deserialize(ref Utf8JsonReader reader, Type targetType, JsonSerializerOptions options)
+    {
+        if (reader.TokenType != JsonTokenType.StartArray)
+            throw new JsonException("Expected JSON array");
+
+        var ctor = targetType.GetConstructors()[0];
+        var parameters = ctor.GetParameters();
+        var args = new object?[parameters.Length];
+
+        reader.Read();
+        for (var i = 0; i < parameters.Length; i++)
+        {
+            args[i] = JsonSerializer.Deserialize(ref reader, parameters[i].ParameterType, options);
+            reader.Read();
+        }
+
+        if (reader.TokenType != JsonTokenType.EndArray)
+            throw new JsonException("Expected EndArray");
+
+        return ctor.Invoke(args);
+    }
+}
